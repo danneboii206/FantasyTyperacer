9b37bfd8a30d1bf0f1cb2b814500aeee82ddc359
Daniel Abu Ramadan <daab24@student.bth.se>
2025-01-11 17:16:48 +0100

    for rasmus to download his stuff

---
+++ cmake-build-debug/CMakeFiles/FantasyTyperacer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FantasyTyperacer.dir/combat.cpp.o"
+  "CMakeFiles/FantasyTyperacer.dir/combat.cpp.o.d"
+  "CMakeFiles/FantasyTyperacer.dir/main.cpp.o"
+  "CMakeFiles/FantasyTyperacer.dir/main.cpp.o.d"
+  "CMakeFiles/FantasyTyperacer.dir/menus.cpp.o"
+  "CMakeFiles/FantasyTyperacer.dir/menus.cpp.o.d"
+  "CMakeFiles/FantasyTyperacer.dir/rooms.cpp.o"
+  "CMakeFiles/FantasyTyperacer.dir/rooms.cpp.o.d"
+  "FantasyTyperacer"
+  "FantasyTyperacer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FantasyTyperacer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
